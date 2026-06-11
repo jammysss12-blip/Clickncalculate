9bee53f99e641d2827aeb9d13da2f35dd56f2578
jammysss12-blip <jammy.sss12@gmail.com>
2026-06-11 11:21:31 +0400

    Update interest-rates-guide.ts

--- src/app/blog/interest-rates-guide/interest-rates-guide.ts
+++ src/app/blog/interest-rates-guide/interest-rates-guide.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-interest-rates-guide',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './interest-rates-guide.html',
-  styleUrl: './interest-rates-guide.scss'
+  styleUrl: './interest-rates-guide.scss',
+  standalone: true,
 })
 export class InterestRatesGuide {
-
-  constructor (private router: Router) {}
-  
-  goToBlog() {
-    this.router.navigate(['/blog']);
-  }
-  
-  Try_Our_Calculators() {
-    this.router.navigate(['/loan-emi-calculator']);
+  constructor(private router: Router, private title: Title, private meta: Meta) {
+    this.title.setTitle('Understanding Interest Rates – Simple Financial Guide | ClickNCalculate');
+    this.meta.updateTag({ name: 'description', content: 'Learn how simple and compound interest rates work, how they affect your loans and savings, and how to calculate them easily.' });
   }
 
+  goToBlog() { this.router.navigate(['/blog']); }
+  Try_Our_Calculators() { this.router.navigate(['/loan-emi-calculator']); }
 }
